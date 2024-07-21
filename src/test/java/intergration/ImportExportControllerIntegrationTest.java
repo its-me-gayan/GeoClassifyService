00fcc6003ac7456b8f95868637fb507d5d9fe1ad
gayan <reachme.gayan@gmail.com>
2024-07-21 13:45:33 +0800

    unit testings

---
+++ src/test/java/intergration/ImportExportControllerIntegrationTest.java
@@ -0,0 +1,86 @@
+package intergration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.natlex.geo.GeoClassifyServiceRunner;
+import org.natlex.geo.entity.ExportJob;
+import org.natlex.geo.helper.EntityToDtoMapper;
+import org.natlex.geo.helper.ResponseGenerator;
+import org.natlex.geo.repository.ExportJobRepository;
+import org.natlex.geo.repository.ImportJobRepository;
+import org.natlex.geo.service.ImportExportAsyncService;
+import org.natlex.geo.service.ImportExportService;
+import org.natlex.geo.service.impl.ImportExportServiceImpl;
+import org.natlex.geo.util.JobStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+/**
+ * Author: Gayan Sanjeewa
+ * User: gayan
+ * Date: 7/21/24
+ * Time: 1:12 PM
+ */
+
+@SpringBootTest(classes = GeoClassifyServiceRunner.class)
+@AutoConfigureMockMvc
+public class ImportExportControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ImportJobRepository importJobRepository;
+
+    @MockBean
+    private ExportJobRepository exportJobRepository;
+
+    @Spy
+    private ImportExportAsyncService importExportAsyncService;
+
+    @Spy
+    private ResponseGenerator responseGenerator;
+
+    @Spy
+    private EntityToDtoMapper entityToDtoMapper;
+
+    @Autowired
+    private ImportExportService importExportService;
+
+    @Test
+    void test_DownloadExportedFile_200_Success() throws Exception {
+
+
+        ExportJob exportJob = ExportJob.builder()
+                .id(1L)
+                .completedAt(LocalDateTime.now())
+                .jobCompletedMessage("File Export success and xlsx file saved")
+                .status(JobStatus.DONE)
+                .createdAt(LocalDateTime.now())
+                .file(new byte[]{116, 101, 115, 116})
+                .fileName("test.xlsx")
+                .build();
+
+        Mockito.when(exportJobRepository.findById(Mockito.any()))
+                .thenReturn(Optional.of(exportJob));
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/export/1/file"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header().string("Content-Disposition", "attachment;filename=test.xlsx"))
+                .andReturn();
+
+        byte[] responseBytes = mvcResult.getResponse().getContentAsByteArray();
+        Assertions.assertEquals("test" , new String(responseBytes));
+    }
+}
--- src/test/java/intergration/SectionControllerIntegrationTest.java
+++ src/test/java/intergration/SectionControllerIntegrationTest.java
@@ -3,32 +3,24 @@ package intergration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.natlex.geo.GeoClassifyServiceRunner;
 import org.natlex.geo.dto.SectionRequest;
-import org.natlex.geo.dto.SectionUpdateRequest;
 import org.natlex.geo.dto.generic.GenericRequest;
-import org.natlex.geo.dto.generic.GenericResponse;
 import org.natlex.geo.entity.Section;
 import org.natlex.geo.helper.DtoToEntityMapper;
 import org.natlex.geo.helper.EntityToDtoMapper;
 import org.natlex.geo.helper.ResponseGenerator;
 import org.natlex.geo.repository.SectionRepository;
 import org.natlex.geo.service.SectionService;
-import org.natlex.geo.util.ResponseMessages;
 import org.natlex.geo.util.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -49,33 +41,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = GeoClassifyServiceRunner.class)
 @AutoConfigureMockMvc
 class SectionControllerIntegrationTest {
+    Section section;
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private SectionService sectionService;
-
     @MockBean
     private SectionRepository sectionRepository;
-
-
     @Autowired
     private ObjectMapper objectMapper;
-
     @Spy
     private ResponseGenerator responseGenerator;
-
     @Spy
     private EntityToDtoMapper entityToDtoMapper;
-
     @Spy
     private DtoToEntityMapper dtoToEntityMapper;
 
-    Section section;
-
     @BeforeEach
     public void setUp() {
-         section = Section.builder()
+        section = Section.builder()
                 .id(UUID.randomUUID().toString())
                 .sectionId(10)
                 .status(Status.ACTIVE)
@@ -92,9 +76,9 @@ class SectionControllerIntegrationTest {
                 .build();
 
         GenericRequest<SectionRequest> sectionRequestGenericRequest = new GenericRequest<>(
-                LocalDateTime.now(),"v1",sectionRequest
+                LocalDateTime.now(), "v1", sectionRequest
         );
-        Mockito.when(sectionRepository.findSectionBySectionIdAndStatusNot(Mockito.anyInt() , Mockito.any()))
+        Mockito.when(sectionRepository.findSectionBySectionIdAndStatusNot(Mockito.anyInt(), Mockito.any()))
                 .thenReturn(Optional.empty());
 
         Mockito.when(sectionRepository.save(Mockito.any()))
@@ -107,6 +91,7 @@ class SectionControllerIntegrationTest {
                 .andExpect(jsonPath("$.data").isNotEmpty());
 
     }
+
     @Test
     void test_AddSection_400_BadRequest_Field_Validation_Failed() throws Exception {
         SectionRequest sectionRequest = SectionRequest.builder()
@@ -114,9 +99,9 @@ class SectionControllerIntegrationTest {
                 .build();
 
         GenericRequest<SectionRequest> sectionRequestGenericRequest = new GenericRequest<>(
-                LocalDateTime.now(),"v1",sectionRequest
+                LocalDateTime.now(), "v1", sectionRequest
         );
-        Mockito.when(sectionRepository.findSectionBySectionIdAndStatusNot(Mockito.anyInt() , Mockito.any()))
+        Mockito.when(sectionRepository.findSectionBySectionIdAndStatusNot(Mockito.anyInt(), Mockito.any()))
                 .thenReturn(Optional.empty());
 
         Mockito.when(sectionRepository.save(Mockito.any()))
@@ -129,53 +114,4 @@ class SectionControllerIntegrationTest {
                 .andExpect(jsonPath("$.fieldError").isNotEmpty())
                 .andExpect(jsonPath("$.fieldError").isArray());
     }
-
-//
-//    @Test
-//    public void testGetAllSections() throws Exception {
-//        GenericResponse expectedResponse = new GenericResponse();
-//        Mockito.when(sectionService.getAllSections()).thenReturn(expectedResponse);
-//
-//        mockMvc.perform(get("/api/v1/section")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").isNotEmpty());
-//    }
-//
-//    @Test
-//    void testGetSectionsByGeoClassCode() throws Exception {
-//        GenericResponse expectedResponse = new GenericResponse();
-//        Mockito.when(sectionService.getAllSectionsByGeoLocCode(Mockito.anyString()))
-//                .thenReturn(expectedResponse);
-//
-//        mockMvc.perform(get("/api/v1/section/by-code")
-//                        .param("code", "TestCode")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").isNotEmpty());
-//    }
-//
-//    @Test
-//    void testUpdateSection() throws Exception {
-//        GenericResponse expectedResponse = new GenericResponse();
-//        Mockito.when(sectionService.updateSection(Mockito.any(SectionUpdateRequest.class)))
-//                .thenReturn(expectedResponse);
-//
-//        mockMvc.perform(patch("/api/v1/section")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(sectionUpdateRequest)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").isNotEmpty());
-//    }
-//
-//    @Test
-//    void testDeleteSection() throws Exception {
-//        GenericResponse expectedResponse = new GenericResponse();
-//        Mockito.when(sectionService.deleteSection(Mockito.anyString())).thenReturn(expectedResponse);
-//
-//        mockMvc.perform(delete("/api/v1/section/{id}", "1")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.data").isNotEmpty());
-//    }
 }
